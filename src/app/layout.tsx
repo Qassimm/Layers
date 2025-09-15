6e49a4883a9bb88f8c8ffbc2fbd1d92efaa41dd7
Muhammad Qasim <muhammadqqaasim@gmail.com>
2025-09-15 17:50:18 +0500

    prj

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body
-        className={`${poppins.variable} antialiased font-sans`}
+        className={`${poppins.variable} antialiased bg-[#0a0a0a] font-sans`}
       >
         {children}
       </body>
